f3ed4ee504993c53a4f97d4a128bca017d59d5d8
adityadipakpatel <adityadipakpatel@gmail.com>
2025-11-06 01:45:02 -0700

    revert all files back to the state of "7b11d66875455f5b0333e6861f4301bf4c1a19dd"(rayu's old code) using "git revert hash" 2 times, using "git rebase -i HEAD~2" with squash
    - try "git diff this_commit_hash 7b11d66875455f5b0333e6861f4301bf4c1a19dd" if you want to test if everything was reverted or not

--- campusnest/src/lib/supabaseClient.ts
+++ campusnest/src/lib/supabaseClient.ts
@@ -1,5 +1,6 @@
-import { createClient } from '@supabase/supabase-js';
-import { EXPO_PUBLIC_SUPABASE_URL, EXPO_PUBLIC_SUPABASE_ANON_KEY } from '@env';
+import { createClient } from '@supabase/supabase-js'
 
-export const supabase = createClient(EXPO_PUBLIC_SUPABASE_URL, EXPO_PUBLIC_SUPABASE_ANON_KEY);
+const SUPABASE_URL = process.env.EXPO_PUBLIC_SUPABASE_URL!
+const SUPABASE_ANON_KEY = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY!
 
+export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
