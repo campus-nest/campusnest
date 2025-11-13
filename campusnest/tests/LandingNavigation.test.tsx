fae60bfdb388b8ee18a65c317faba6362feb797e
Aditya Patel <adityadipakpatel@gmail.com>
2025-11-13 02:44:41 -0700

    landing page tests (#61)
    * landing page tests
    
    * Refactor SVG mocks in LandingScreen tests
    
    Updated SVG mock imports for LandingScreen tests.
    
    * Update LandingScreen.test.tsx
    
    * Remove duplicate comment for SVG mocks

--- campusnest/tests/LandingNavigation.test.tsx
+++
@@ -1,7 +0,0 @@
-import { render } from "@testing-library/react-native";
-import { Text } from "react-native";
-
-test("renders", () => {
-  const { getByText } = render(<Text>Hello, CampusNest!</Text>);
-  expect(getByText("Hello, CampusNest!")).toBeTruthy();
-});
