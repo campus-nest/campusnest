c89e600fa6eeb6779d3ded4dfc7ab49c59e0ad6b
siddhantgoel7 <siddhantgoel5@gmail.com>
2025-11-14 19:25:36 -0700

    mount expo-router

--- campusnest/App.tsx
+++ campusnest/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import { PaperProvider } from 'react-native-paper';
-import AppNavigator from './src/navigation/AppNavigator';
+import { Slot } from "expo-router";
+import { PaperProvider } from "react-native-paper";
 
 export default function App() {
   return (
     <PaperProvider>
-      <AppNavigator />
+      <Slot /> 
     </PaperProvider>
   );
 }
